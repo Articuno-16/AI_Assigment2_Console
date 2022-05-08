cb55d958206d6458d40b552642324f2f64590c9c
Articuno-16 <68211849+Articuno-16@users.noreply.github.com>
2022-05-08 15:57:44 +0700

    add start function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from table import Table
 
 box = Table()
 
-box.movingTurn(player='player1', index=4, direction='Right')
-box.movingTurn(player='player2', index=10, direction='Right')
-
+# box.movingTurn(player='player1', index=2, direction='Right')
+# box.movingTurn(player='player2', index=10, direction='Right')
+box.start()
 
