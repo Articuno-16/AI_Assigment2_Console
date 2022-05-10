f02f0b7a59a9f9de48187d40aff777ed7314ea1f
02david20 <davidhuynh0222@gmail.com>
2022-05-10 18:48:06 +0700

    fix twice messagebox, cant choice after noQuan, add goFirst()

--- main copy.py
+++ main copy.py
@@ -1,5 +1,6 @@
 from game import Game
-
+import pygame
+import sys
 if __name__ == "__main__":
     print("+----- O AN QUAN -----+\n\n")
 
@@ -8,6 +9,18 @@ if __name__ == "__main__":
     # game = Game(algo_0="random", algo_1="expectimax")
     # game = Game(algo_0="random",algo_1="alpha_beta")
     # game = Game(algo_0="random",algo_1="random")
-    
-    game.run()
-
+    while True:        
+        game.run()
+        reset = False
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_r:
+                        game.reset()
+                        reset = True
+                        break
+            if reset:
+                break
